cf9ca31ae9a26bad4b8091c34034f5951bd03f08
VictorGalkevich <2vektor54@gmail.com>
2024-05-20 18:41:44 +0300

    added AND command

--- src/main/java/cachevg/db/processor/AndCommandProcessor.java
+++ src/main/java/cachevg/db/processor/AndCommandProcessor.java
@@ -1,9 +1,24 @@
 package cachevg.db.processor;
 
+import cachevg.db.storage.InitialStorage;
+import cachevg.db.types.AbstractValue;
+import cachevg.db.types.DefaultString;
+
 public class AndCommandProcessor implements Processor {
     @Override
     public String process(String[] args) {
-        //TODO: implement
-        return null;
+        if (args.length < 3) {
+            String internal = String.join(" ", args);
+            return "Command format must be: [key1] AND [key2], instead got: %s".formatted(internal);
+        }
+        AbstractValue<?> obj = InitialStorage.instance().getObj(args[0]);
+        if (!(obj instanceof DefaultString) || obj.getValue() == null) {
+            return "No value persistent by key: %s".formatted(args[0]);
+        }
+        String second = InitialStorage.instance().get(args[2]);
+        if (second == null) {
+            return "No value persistent by key: %s".formatted(args[1]);
+        }
+        return ((DefaultString) obj).conjunction(second);
     }
 }
