89932952e17566a93e7d076703a16d8ab22a3e4a
VictorGalkevich <2vektor54@gmail.com>
2024-05-21 09:00:15 +0300

    added AND, APPEND, EVEN, FLIP, GBIT, OR, SBIT, XOR commands

--- src/main/java/cachevg/db/processor/GbitCommandProcessor.java
+++ src/main/java/cachevg/db/processor/GbitCommandProcessor.java
@@ -1,9 +1,29 @@
 package cachevg.db.processor;
 
+import cachevg.db.storage.InitialStorage;
+import cachevg.db.types.AbstractValue;
+import cachevg.db.types.DefaultString;
+
 public class GbitCommandProcessor implements Processor {
     @Override
     public String process(String[] args) {
-        //TODO: implement
-        return null;
+        if (args.length < 3) {
+            String internal = String.join(" ", args);
+            return "Command format must be: GET [key] [index], instead got: %s".formatted(internal);
+        }
+        AbstractValue<?> obj = InitialStorage.instance().getObj(args[1]);
+        if (!(obj instanceof DefaultString) || obj.getValue() == null) {
+            return "No value persistent by key: %s".formatted(args[0]);
+        }
+        int val = 0;
+        try {
+            val = Integer.parseInt(args[2]);
+            if (val < 0) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            return "[index] might be a uint32_t value";
+        }
+        return String.valueOf(((DefaultString) obj).getBit(val));
     }
 }
--- src/main/java/cachevg/db/processor/XorCommandProcessor.java
+++ src/main/java/cachevg/db/processor/XorCommandProcessor.java
@@ -1,9 +1,14 @@
 package cachevg.db.processor;
 
+import static cachevg.db.processor.OrCommandProcessor.processFiltered;
+
 public class XorCommandProcessor implements Processor {
     @Override
     public String process(String[] args) {
-        //TODO: implement
-        return null;
+        if (args.length < 3) {
+            String internal = String.join(" ", args);
+            return "Command format must be: [key1] XOR [key2], instead got: %s".formatted(internal);
+        }
+        return processFiltered(args);
     }
 }
